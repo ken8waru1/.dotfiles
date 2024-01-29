5f8613f7b5b958d36e16d08211e7583510e70c5f
Ken Ha <ken8waru1@gmail.com>
2024-01-29 14:32:43 -0800

    Change tab formatting

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -3,6 +3,12 @@
 -- Add any additional options here
 vim.opt.winbar = "%=%m %f"
 vim.g.autoformat = false
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.wrap = false
+vim.opt.swapfile = false
 
 --- HACK: Fixes a visual issue with the noice cmdline colors since the gruxbox theme doesn't quite support the plugin just yet, can possibly remove in the future
 vim.cmd([[ hi! NoiceCmdlinePopup guibg=#3c3836 ]])
