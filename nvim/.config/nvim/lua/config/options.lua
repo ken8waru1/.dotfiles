ba6619776006231f5de1829cac476f5e92f87819
Ken Ha <ken8waru1@gmail.com>
2024-01-31 00:46:31 -0800

    Move trouble.nvim and noice.nvim hl overrides to colorscheme plugin file

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -9,7 +9,3 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.wrap = false
 vim.opt.swapfile = false
-
---- HACK: Fixes a visual issue with the noice cmdline colors since the gruxbox theme doesn't quite support the plugin just yet, can possibly remove in the future
-vim.cmd([[ hi! NoiceCmdlinePopup guibg=#3c3836 ]])
-vim.cmd([[ hi! NoiceCmdlinePopupBorder guibg=#3c3836 ]])
--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,34 +1,40 @@
 return {
-    -- add gruvbox
-    {
-        "ellisonleao/gruvbox.nvim",
-        opts = {
-            italic = {
-                strings = false,
-                emphasis = true,
-                comments = true,
-                operators = false,
-                folds = true,
-            },
-            overrides = {
-                NormalFloat = { fg = "#ebdbb2", bg = "#282828" },
-            },
-        },
+  -- add gruvbox
+  {
+    "ellisonleao/gruvbox.nvim",
+    opts = {
+      italic = {
+        strings = false,
+        emphasis = true,
+        comments = true,
+        operators = false,
+        folds = true,
+      },
+      overrides = {
+        NormalFloat = { fg = "#ebdbb2", bg = "#282828" },
+        TroubleSignInformation = { fg = "#83a598", bg = "NONE" },
+        TroubleFoldIcon = { fg = "#fabd2f", bg = "NONE" },
+        NoiceCmdlinePopup = { bg = "#282828" },
+        NoiceCmdlinePopupBorder = { bg = "#282828" },
+        NoiceCmdlinePopupTitle = { fg = "#83a598", bg = "NONE" },
+        NoiceCmdlineIcon = { fg = "#83a598", bg = "#282828" },
+      },
     },
-    {
-        "folke/tokyonight.nvim",
-        priority = 1000,
-        opts = {},
-    },
-    {
-        "Shatur/neovim-ayu",
-    },
+  },
+  {
+    "folke/tokyonight.nvim",
+    priority = 1000,
+    opts = {},
+  },
+  {
+    "Shatur/neovim-ayu",
+  },
 
-    -- Configure LazyVim to load gruvbox
-    {
-        "LazyVim/LazyVim",
-        opts = {
-            colorscheme = "gruvbox",
-        },
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
     },
+  },
 }
