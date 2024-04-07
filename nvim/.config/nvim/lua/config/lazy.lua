40ec8c817a4b98d099b85410f3f533b6a208a67d
Ken Ha <ken8waru1@gmail.com>
2024-04-07 03:45:23 -0700

    Update plugins

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -20,6 +20,7 @@ require("lazy").setup({
     },
     ui = {
         border = "single",
+        backdrop = 100,
     },
     defaults = {
         -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
