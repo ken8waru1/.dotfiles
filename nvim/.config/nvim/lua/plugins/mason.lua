4b14e2a35eb4bebfeb20e21dd7bd1faeb3919a64
Ken Ha <ken8waru1@gmail.com>
2024-11-20 16:27:31 -0800

    Try out typescript tools in place of tsserver

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -3,7 +3,7 @@ return {
         "williamboman/mason.nvim",
         opts = function(_, opts)
             vim.list_extend(opts.ensure_installed, {
-                "typescript-language-server",
+                -- "typescript-language-server",
                 "rust-analyzer",
                 "bash-language-server",
                 "yaml-language-server",
