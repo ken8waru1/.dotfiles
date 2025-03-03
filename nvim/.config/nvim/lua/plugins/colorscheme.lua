7b9602371a731260a2469f6b380eb1f4beea394a
Ken Ha <ken8waru1@gmail.com>
2025-03-03 07:44:39 -0700

    Update plugins

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,42 +1,46 @@
 return {
     -- add gruvbox
-    {
-        "ellisonleao/gruvbox.nvim",
-        opts = {
-            italic = {
-                strings = false,
-                emphasis = true,
-                comments = true,
-                operators = false,
-                folds = true,
-            },
-            overrides = {
-                NormalFloat = { fg = "#ebdbb2", bg = "#282828" },
-                GruvboxRedSign = { fg = "#fb4934", bg = "NONE" },
-                GruvboxAquaSign = { fg = "#83a598", bg = "NONE" },
-                GruvboxBlueSign = { fg = "#83a598", bg = "NONE" },
-                GruvboxYellowSign = { fg = "#fabd2f", bg = "NONE" },
-                TroubleTextError = { fg = "#ebdbb2", bg = "NONE" },
-                TroubleTextInformation = { fg = "#ebdbb2", bg = "NONE" },
-                TroubleTextWarning = { fg = "#ebdbb2", bg = "NONE" },
-                TroubleTextHint = { fg = "#ebdbb2", bg = "NONE" },
-                TroubleFoldIcon = { fg = "#fabd2f", bg = "NONE" },
-                FlashBackdrop = { link = "NONE" },
-            },
-        },
-    },
+    -- {
+    --     "ellisonleao/gruvbox.nvim",
+    --     opts = {
+    --         italic = {
+    --             strings = false,
+    --             emphasis = true,
+    --             comments = true,
+    --             operators = false,
+    --             folds = true,
+    --         },
+    --         overrides = {
+    --             NormalFloat = { fg = "#ebdbb2", bg = "#282828" },
+    --             GruvboxRedSign = { fg = "#fb4934", bg = "NONE" },
+    --             GruvboxAquaSign = { fg = "#83a598", bg = "NONE" },
+    --             GruvboxBlueSign = { fg = "#83a598", bg = "NONE" },
+    --             GruvboxYellowSign = { fg = "#fabd2f", bg = "NONE" },
+    --             TroubleTextError = { fg = "#ebdbb2", bg = "NONE" },
+    --             TroubleTextInformation = { fg = "#ebdbb2", bg = "NONE" },
+    --             TroubleTextWarning = { fg = "#ebdbb2", bg = "NONE" },
+    --             TroubleTextHint = { fg = "#ebdbb2", bg = "NONE" },
+    --             TroubleFoldIcon = { fg = "#fabd2f", bg = "NONE" },
+    --             FlashBackdrop = { link = "NONE" },
+    --         },
+    --     },
+    -- },
     {
         "folke/tokyonight.nvim",
         lazy = false,
         opts = {
             transparent = true,
+            styles = {
+                sidebars = "transparent",
+                floats = "transparent",
+            },
         },
     },
-    -- Configure LazyVim to load gruvbox
+    -- Configure LazyVim to load theme
     {
         "LazyVim/LazyVim",
         opts = {
-            colorscheme = "gruvbox",
+            colorscheme = "tokyonight",
         },
     },
 }
