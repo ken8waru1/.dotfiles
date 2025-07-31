eb2eabe5d1acd49a3c267913c2461a3dd8595a5a
ken8waru1 <ken8waru1@gmail.com>
2025-07-30 17:20:19 -0700

    Switch back to lazy until custom config is caught up

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -1,6 +1,7 @@
 return {
     {
-        "williamboman/mason.nvim",
+        { "mason-org/mason.nvim", version = "^1.0.0" },
+        { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
         opts = function(_, opts)
             vim.list_extend(opts.ensure_installed, {
                 -- "typescript-language-server",
--- nvim_lazy/.config/nvim/README.md
+++
@@ -1,1 +0,0 @@
-My personal Neovim configurations using the [LazyVim](https://github.com/LazyVim/LazyVim) setup as a starting template. 
